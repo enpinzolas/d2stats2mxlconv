1ee802b85305ffff1c6e5f47b8240c9e4dc3a7d5
Eneko Pinzoals <en.pinzolas@gmail.com>
2025-07-12 22:20:54 +0200

    deployment fixes

--- src/utils/FilterConverter.ts
+++ src/utils/FilterConverter.ts
@@ -4,7 +4,7 @@ import ItemFilterRuleTemplate from "../data/ItemFilterRuleTemplate.json";
 
 const convertFilter = (filterContent: string): ItemFilterRule[] => {
   const matchingCodes = new Set<ItemFilterRule>();
-  filterContent.split(/\r?\n/).forEach((line, index) => {
+  filterContent.split(/\r?\n/).forEach((line) => {
     if (line.length > 0) {
       //* Clean filters
       const [catstring, regex] = line.slice(0, line.length - 1).split(/ "/);
