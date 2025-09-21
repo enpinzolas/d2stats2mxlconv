cf6d4ee244d699c7f0b55b4a14e3eb6d6584f265
Oksana Klochak <oksana.klochak@gmail.com>
2025-09-21 19:04:28 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# d2stats2mxlconv
-The code for the future github page that will convert a d2stats filter into a medianXL ingame filter.
+# Portfolio
--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,13 @@
-import { useState, useEffect } from "react";
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import json from "react-syntax-highlighter/dist/esm/languages/hljs/json";
-import style from "react-syntax-highlighter/dist/esm/styles/hljs/darcula";
 import "./App.css";
-import ItemFilterTemplate from "./data/ItemFilterTemplate.json";
-import type { ItemFilter } from "./vite-env";
-import { convertFilter } from "./utils/FilterConverter";
-import textContent from "./data/exampleInput.txt?raw";
 
-SyntaxHighlighter.registerLanguage("json", json);
 
 function App() {
-  const [fileContent, setFileContent] = useState<string>(textContent);
-  const [jsonContent, setJsonContent] = useState<string>(JSON.stringify(ItemFilterTemplate, null, 2));
-  const [fileName, setFileName] = useState<string>("modified.json");
-
-  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    setFileName(file.name.replace(/\.[^/.]+$/, "") + ".json");
-
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const text = event.target?.result as string;
-      setFileContent(text);
-    };
-    reader.readAsText(file);
-  };
-
-  const handleTransform = () => {
-    console.log(ItemFilterTemplate);
-    const filterJson: ItemFilter = structuredClone(ItemFilterTemplate);
-    const newRules = convertFilter(fileContent);
-    filterJson.rules.push(...newRules);
-
-    setJsonContent(JSON.stringify(filterJson, null, 2));
-  };
-
-  const handleDownload = () => {
-    const blob = new Blob([jsonContent], { type: "text/json" });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = fileName;
-    link.click();
-    URL.revokeObjectURL(url);
-  };
-
-  useEffect(handleTransform, [fileContent]);
 
   return (
     <>
-      <h1>Text File Editor</h1>
+      <h1>Portfolio</h1>
       <div className="container">
-        <div className="datablock">
-          <input className="clickable" type="file" accept=".txt" onChange={handleFileUpload} />
-
-          <textarea
-            className="content"
-            value={fileContent}
-            onChange={(e) => {
-              setFileContent(e.target.value);
-            }}
-            wrap="off"
-            placeholder="Your file content will appear here..."
-          />
-        </div>
-
-        <div className="datablock">
-          <div>
-            <button className="json-style" onClick={handleDownload}>
-              Download Filter
-            </button>
-          </div>
-          <SyntaxHighlighter className="content" language="json" style={style}>
-            {jsonContent.substring(0, 5000)}
-          </SyntaxHighlighter>
-        </div>
+       Hiiiii
       </div>
     </>
   );
